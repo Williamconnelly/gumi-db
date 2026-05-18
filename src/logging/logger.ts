ea7b2d2a28b2cffdc59451281546ec95050867b2
Williamconnelly <williamconnelly.dev@gmail.com>
2026-05-17 18:36:28 -0700

    Centralizing logger files

--- src/logging/logger.ts
+++ src/logging/logger.ts
@@ -1,5 +1,4 @@
 import { ITimerStop } from './interfaces';
-import { ScopedLogger } from './scoped-logger';
 
 export class Logger {
 
@@ -83,6 +82,37 @@ export class Logger {
     const fnStub: string = fnName ? `.${fnName}` : '';
 
     return `[${this.className}${fnStub}]: ${message}`;
+  }
+
+}
+
+export class ScopedLogger extends Logger {
+
+  constructor(
+    className: string,
+    protected readonly fnName: string
+  ) {
+    super(className);
+  }
+
+  public override info(message: string): void {
+    super.info(message);
+  }
+
+  public override warn(message: string): void {
+    super.warn(message);
+  }
+
+  public override error(message: string): void {
+    super.error(message);
+  }
+
+  public override debug(message: string): void {
+    super.info(message);
+  }
+
+  protected override format(message: string): string {
+    return `[${this.className}.${this.fnName}]: ${message}`;
   }
 
 }
